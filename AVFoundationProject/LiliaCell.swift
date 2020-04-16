4842ad71d894af93d30d03933ce46769317c1ebd
Lilia Yudina <liliayudina@gmail.com>
2020-04-16 15:05:03 -0400

    added storyboard

---
+++ AVFoundationProject/LiliaCell.swift
@@ -0,0 +1,13 @@
+//
+//  LiliaCell.swift
+//  AVFoundationProject
+//
+//  Created by Lilia Yudina on 4/16/20.
+//  Copyright © 2020 Ahad Islam. All rights reserved.
+//
+
+import UIKit
+
+class LiliaCell: UICollectionViewCell {
+    
+}
